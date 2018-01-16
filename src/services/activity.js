2b0da3b64237a9baca8917e5e090c634bf7304d2
diroguanMac <diroguan@foxmail.com>
2018-01-16 21:35:11 +0800

    activity后台

--- src/services/activity.js
+++ src/services/activity.js
@@ -2,17 +2,17 @@ import { stringify } from 'qs';
 import request from '../utils/request';
 import { md5Sign } from '../utils/utils';
 
-// export async function queryHikeActivities(params) {
-//   return request(`/api/hike_activities?${stringify(params)}`);
-// }
-
 export async function queryHikeActivities(params) {
-  const sign = md5Sign({ service: 'Event.GetEventList' });
-  return request(`/sysuhikerapi/?service=Event.GetEventList&sign=${sign}`, {
-    method: 'POST',
-    body: JSON.stringify({ ...params, sign }),
-  });
+  return request(`/api/hike_activities?${stringify(params)}`);
 }
+
+// export async function queryHikeActivities(params) {
+//   const sign = md5Sign({ service: 'Event.GetEventList' });
+//   return request(`/sysuhikerapi/?service=Event.GetEventList&sign=${sign}`, {
+//     method: 'POST',
+//     body: JSON.stringify({ ...params }),
+//   });
+// }
 
 export async function getActivity(params) {
   return request('/api/get_hike_activity_byid', {
@@ -23,6 +23,16 @@ export async function getActivity(params) {
     },
   });
 }
+// export async function getActivity(params) {
+//   const sign = md5Sign({ service: 'Event.GetEventInfo' });
+//   return request(`/sysuhikerapi/?service=Event.GetEventInfo&sign=${sign}`, {
+//     method: 'POST',
+//     body: {
+//       ...params,
+//       method: 'post',
+//     },
+//   });
+// }
 
 export async function getMemberList(params) {
   return request('/api/get_member_list_byid', {
@@ -33,6 +43,14 @@ export async function getMemberList(params) {
     },
   });
 }
+// export async function getMemberList(params) {
+//   const sign = md5Sign({ service: 'Event.GetEventJoinList' });
+//   return request(`/sysuhikerapi/?service=Event.GetEventJoinList&sign=${sign}`, {
+//     method: 'POST',
+//     body: JSON.stringify(params),
+//   });
+// }
+
 export async function getReList(params) {
   return request('/api/get_re_list_byid', {
     method: 'POST',
@@ -42,3 +60,15 @@ export async function getReList(params) {
     },
   });
 }
+
+/**
+ * 获取活动的评论列表
+ * @param  { dict } params {event_id: number}
+ */
+// export async function getReList(params) {
+//   const sign = md5Sign({ service: 'Event.GetEventJoinList' });
+//   return request(`/sysuhikerapi/?service=Event.GetEventReList&sign=${sign}`, {
+//     method: 'POST',
+//     body: JSON.stringify(params),
+//   });
+// }
